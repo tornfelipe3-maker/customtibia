277f66c3afa59b7e1247fc15246b2631596d5cc3
Felipe <seu_email_aqui@exemplo.com tornfelipe3@gmail.com>
2025-12-23 20:01:13 -0300

    fiz tal melhoria

--- constants/items/ammunition.ts
+++ constants/items/ammunition.ts
@@ -10,20 +10,20 @@ export const AMMUNITION_LIST: Item[] = [
   { id: 'bolt', name: 'Bolt', type: 'equipment', slot: EquipmentSlot.AMMO, attack: 18, price: 4, sellPrice: 1, soldTo: [NpcType.TRADER], description: 'Munição para Bestas (Tier 2).', ammoType: 'bolt', image: `${IMG_BASE}Bolt.gif`, requiredLevel: 8 },
   
   // TIER 3 - Sniper Arrow
-  { id: 'sniper_arrow', name: 'Sniper Arrow', type: 'equipment', slot: EquipmentSlot.AMMO, attack: 28, price: 10, sellPrice: 3, soldTo: [NpcType.TRADER], description: 'Flecha precisa (Tier 3).', ammoType: 'arrow', image: `${IMG_BASE}Sniper_Arrow.gif`, requiredLevel: 20 },
+  { id: 'sniper_arrow', name: 'Sniper Arrow', type: 'equipment', slot: EquipmentSlot.AMMO, attack: 30, price: 10, sellPrice: 3, soldTo: [NpcType.TRADER], description: 'Flecha precisa (Tier 3).', ammoType: 'arrow', image: `${IMG_BASE}Sniper_Arrow.gif`, requiredLevel: 20 },
 
   // TIER 4 - Onyx Arrow
-  { id: 'onyx_arrow', name: 'Onyx Arrow', type: 'equipment', slot: EquipmentSlot.AMMO, attack: 40, price: 20, sellPrice: 5, soldTo: [NpcType.TRADER], description: 'Flecha de ônix (Tier 4).', ammoType: 'arrow', image: `${IMG_BASE}Onyx_Arrow.gif`, requiredLevel: 40 },
+  { id: 'onyx_arrow', name: 'Onyx Arrow', type: 'equipment', slot: EquipmentSlot.AMMO, attack: 45, price: 20, sellPrice: 5, soldTo: [NpcType.TRADER], description: 'Flecha de ônix (Tier 4).', ammoType: 'arrow', image: `${IMG_BASE}Onyx_Arrow.gif`, requiredLevel: 40 },
 
   // TIER 5 - Power Bolt
-  { id: 'power_bolt', name: 'Power Bolt', type: 'equipment', slot: EquipmentSlot.AMMO, attack: 55, price: 35, sellPrice: 10, soldTo: [NpcType.TRADER], description: 'Poderoso (Tier 5).', ammoType: 'bolt', image: `${IMG_BASE}Power_Bolt.gif`, requiredLevel: 70 },
+  { id: 'power_bolt', name: 'Power Bolt', type: 'equipment', slot: EquipmentSlot.AMMO, attack: 65, price: 35, sellPrice: 10, soldTo: [NpcType.TRADER], description: 'Poderoso (Tier 5).', ammoType: 'bolt', image: `${IMG_BASE}Power_Bolt.gif`, requiredLevel: 70 },
 
   // TIER 6 - Prismatic Bolt
-  { id: 'prismatic_bolt', name: 'Prismatic Bolt', type: 'equipment', slot: EquipmentSlot.AMMO, attack: 75, price: 50, sellPrice: 15, soldTo: [NpcType.BLUE_DJINN], description: 'Besta prismática (Tier 6).', ammoType: 'bolt', requiredLevel: 90, image: `${IMG_BASE}Prismatic_Bolt.gif` },
+  { id: 'prismatic_bolt', name: 'Prismatic Bolt', type: 'equipment', slot: EquipmentSlot.AMMO, attack: 85, price: 50, sellPrice: 15, soldTo: [NpcType.BLUE_DJINN], description: 'Besta prismática (Tier 6).', ammoType: 'bolt', requiredLevel: 90, image: `${IMG_BASE}Prismatic_Bolt.gif` },
 
   // TIER 7 - Spectral Bolt
-  { id: 'spectral_bolt', name: 'Spectral Bolt', type: 'equipment', slot: EquipmentSlot.AMMO, attack: 95, price: 0, sellPrice: 20, soldTo: [NpcType.RASHID], description: 'Munição fantasma (Tier 7).', ammoType: 'bolt', requiredLevel: 150, image: `${IMG_BASE}Spectral_Bolt.gif` },
+  { id: 'spectral_bolt', name: 'Spectral Bolt', type: 'equipment', slot: EquipmentSlot.AMMO, attack: 105, price: 0, sellPrice: 20, soldTo: [NpcType.RASHID], description: 'Munição fantasma (Tier 7).', ammoType: 'bolt', requiredLevel: 150, image: `${IMG_BASE}Spectral_Bolt.gif` },
 
   // TIER 8 - Diamond Arrow
-  { id: 'diamond_arrow', name: 'Diamond Arrow', type: 'equipment', slot: EquipmentSlot.AMMO, attack: 120, price: 100, sellPrice: 30, soldTo: [NpcType.RASHID], description: 'Flecha de área (Tier 8).', ammoType: 'arrow', requiredLevel: 300, image: `${IMG_BASE}Diamond_Arrow.gif` },
+  { id: 'diamond_arrow', name: 'Diamond Arrow', type: 'equipment', slot: EquipmentSlot.AMMO, attack: 135, price: 100, sellPrice: 30, soldTo: [NpcType.RASHID], description: 'Flecha de área (Tier 8).', ammoType: 'arrow', requiredLevel: 300, image: `${IMG_BASE}Diamond_Arrow.gif` },
 ];
